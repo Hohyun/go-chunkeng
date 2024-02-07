c188357e7c01da92f9639d573ad9090088db4f0f
Hohyun Kim <skykim63@gmail.com>
2024-02-07 20:59:15 +0900

    working new score api

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Hohyun/go-chunkeng/internal/score"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -30,6 +31,7 @@ func main() {
 	}
 
 	app := fiber.New()
+	app.Use(cors.New())
 
 	setupRoutes(app)
 
--- internal/score/api_score.go
+++ internal/score/api_score.go
@@ -4,37 +4,44 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/Hohyun/go-chunkeng/internal/util"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
+	fiberlog "github.com/gofiber/fiber/v2/log"
 )
 
 type Score struct {
-	ID         int    `json:"id"`
+	ID         int       `json:"id"`
 	TestDate   time.Time `json:"test_date"`
-	TestName   string `json:"test_name"`
-	MemberID   string `json:"member_id"`
-	MemberName string `json:"member_name"`
-	Team       string `json:"team"`
-	Subject    string `json:"subject"`
-	Teacher    string `json:"teacher"`
-	ErrCnt     int    `json:"err_cnt"`
-	TtlCnt     int    `json:"ttl_cnt"`
-	Chaewoom   bool   `json:"chaewoom"`
-	RegID      string `json:"reg_id"`
-	RegDate    time.Time  `json:"reg_date"`
-	ModDate    time.Time  `json:"mod_date"`
-	Remarks    string `json:"remarks"`
+	TestName   string    `json:"test_name"`
+	MemberID   string    `json:"member_id"`
+	MemberName string    `json:"member_name"`
+	Team       string    `json:"team"`
+	Subject    string    `json:"subject"`
+	Teacher    string    `json:"teacher"`
+	ErrCnt     int       `json:"err_cnt"`
+	TtlCnt     int       `json:"ttl_cnt"`
+	Chaewoom   bool      `json:"chaewoom"`
+	RegID      string    `json:"reg_id"`
+	RegDate    time.Time `json:"reg_date"`
+	ModDate    time.Time `json:"mod_date"`
+	Remarks    string    `json:"remarks"`
+}
+
+type ScoreWithChaewoom struct {
+	Scores           []Score `json:"scores"`
+	ChaewoomCriteria int     `json:"chaewoomCriteria"`
 }
 
 func GetScores(c *fiber.Ctx) error {
 	dsn := util.GetMysqlDsn()
 	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		panic(err)
-	}
+	checkError(err)
+
+	defer db.Close()
 
 	queryString := `
 	select id, test_date, test_name, member_id, member_name, team, subject, teacher, err_cnt, ttl_cnt, chaewoom, reg_id, reg_date, mod_date, remarks
@@ -47,7 +54,7 @@ func GetScores(c *fiber.Ctx) error {
 	var r Score
 	rows, err := db.Query(queryString)
 	for rows.Next() {
-		err := rows.Scan(&r.ID, &r.TestDate, &r.TestName, &r.MemberID, &r.MemberName, &r.Team, &r.Subject, &r.Teacher, 
+		err := rows.Scan(&r.ID, &r.TestDate, &r.TestName, &r.MemberID, &r.MemberName, &r.Team, &r.Subject, &r.Teacher,
 			&r.ErrCnt, &r.TtlCnt, &r.Chaewoom, &r.RegID, &r.RegDate, &r.ModDate, &r.Remarks)
 		if err != nil {
 			fmt.Printf("log.Logger: %v\n", err.Error())
@@ -70,9 +77,63 @@ func GetScore(c *fiber.Ctx) error {
 }
 
 func NewScore(c *fiber.Ctx) error {
-	return c.SendString("New score")
+	p := new(ScoreWithChaewoom)
+
+	if err := c.BodyParser(p); err != nil {
+		log.Println(err)
+		return err
+	}
+
+	log.Println(p)
+
+	dsn := util.GetMysqlDsn()
+	db, err := sql.Open("mysql", dsn)
+	checkError(err)
+
+	defer db.Close()
+
+	queryString := `
+	insert into ceng_test_score (test_date, test_name, member_id, member_name, team, subject, teacher, err_cnt, ttl_cnt, chaewoom, reg_id, reg_date, mod_date)
+	values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+	`
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Panic(err)
+	}
+	defer tx.Rollback()
+
+	chaewoom := false
+	for _, s := range *&p.Scores {
+		if s.Chaewoom != true || s.ErrCnt >= *&p.ChaewoomCriteria {
+			chaewoom = true
+		}
+		_, err = tx.Exec(queryString, s.TestDate, s.TestName, s.MemberID, s.MemberName,
+			s.Team, s.Subject, s.Teacher, s.ErrCnt, s.TtlCnt, chaewoom,
+			s.RegID, s.RegDate, s.ModDate)
+		checkError(err)
+	}
+	err = tx.Commit()
+	checkError(err)
+
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"result": "FAIL",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"result": "OK",
+	})
 }
 
 func DeleteScore(c *fiber.Ctx) error {
 	return c.SendString("Delete score")
+}
+
+func checkError(err error) {
+	// if err != nil {
+	// 	panic(err)
+	// }
+	fiberlog.Warn("Error: ", err)
 }
