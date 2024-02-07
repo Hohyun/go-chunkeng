aefb05815ed4c6e5985d373577d9bbe1e08ce1c5
Hohyun Kim <skykim63@gmail.com>
2024-02-07 11:29:56 +0900

    change score struct

--- internal/score/api_score.go
+++ internal/score/api_score.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/Hohyun/go-chunkeng/internal/util"
 	_ "github.com/go-sql-driver/mysql"
@@ -11,16 +12,20 @@ import (
 )
 
 type Score struct {
-	TestDate   string `json:"test_date"`
+	ID         int    `json:"id"`
+	TestDate   time.Time `json:"test_date"`
 	TestName   string `json:"test_name"`
 	MemberID   string `json:"member_id"`
 	MemberName string `json:"member_name"`
 	Team       string `json:"team"`
 	Subject    string `json:"subject"`
 	Teacher    string `json:"teacher"`
-	ErrorCount string `json:"err_cnt"`
-	TotalCount string `json:"ttl_cnt"`
+	ErrCnt     int    `json:"err_cnt"`
+	TtlCnt     int    `json:"ttl_cnt"`
 	Chaewoom   bool   `json:"chaewoom"`
+	RegID      string `json:"reg_id"`
+	RegDate    time.Time  `json:"reg_date"`
+	ModDate    time.Time  `json:"mod_date"`
 	Remarks    string `json:"remarks"`
 }
 
@@ -32,17 +37,18 @@ func GetScores(c *fiber.Ctx) error {
 	}
 
 	queryString := `
-	select t_date, t_name, m_id, m_name, team, subject, teacher, err_cnt, ttl_cnt, chaewoom, remarks
+	select id, test_date, test_name, member_id, member_name, team, subject, teacher, err_cnt, ttl_cnt, chaewoom, reg_id, reg_date, mod_date, remarks
 	from ceng_test_score
 	where t_date > DATE_SUB(NOW(), INTERVAL 30 DAY)
-	order by t_date desc, team, subject, test_name, member_name 
+	order by t_date desc, team, test_name, member_name 
 	`
 
 	var rr []Score
 	var r Score
 	rows, err := db.Query(queryString)
 	for rows.Next() {
-		err := rows.Scan(&r.TestDate, &r.MemberID, &r.MemberName, &r.Team, &r.Subject, &r.Teacher, &r.ErrorCount, &r.TotalCount, &r.Chaewoom, &r.Remarks)
+		err := rows.Scan(&r.ID, &r.TestDate, &r.TestName, &r.MemberID, &r.MemberName, &r.Team, &r.Subject, &r.Teacher, 
+			&r.ErrCnt, &r.TtlCnt, &r.Chaewoom, &r.RegID, &r.RegDate, &r.ModDate, &r.Remarks)
 		if err != nil {
 			fmt.Printf("log.Logger: %v\n", err.Error())
 			return c.SendString(err.Error())
