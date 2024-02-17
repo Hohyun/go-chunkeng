3131902a45e6736520ea698531528dbb424094b1
Hohyun Kim <skykim63@gmail.com>
2024-02-17 19:40:47 +0900

    bug fix score api..

--- internal/score/api_score.go
+++ internal/score/api_score.go
@@ -14,7 +14,7 @@ import (
 )
 
 type Score struct {
-	ID         int       `json:"id"`
+	ID         int64     `json:"id"`
 	TestDate   time.Time `json:"test_date"`
 	TestName   string    `json:"test_name"`
 	MemberID   string    `json:"member_id"`
@@ -22,18 +22,19 @@ type Score struct {
 	Team       string    `json:"team"`
 	Subject    string    `json:"subject"`
 	Teacher    string    `json:"teacher"`
-	ErrCnt     int       `json:"err_cnt"`
-	TtlCnt     int       `json:"ttl_cnt"`
+	ErrCnt     int64     `json:"err_cnt"`
+	TtlCnt     int64     `json:"ttl_cnt"`
 	Chaewoom   bool      `json:"chaewoom"`
 	RegID      string    `json:"reg_id"`
 	RegDate    time.Time `json:"reg_date"`
+	ModID      string    `json:"mod_id"`
 	ModDate    time.Time `json:"mod_date"`
 	Remarks    string    `json:"remarks"`
 }
 
 type ScoreWithChaewoom struct {
 	Scores           []Score `json:"scores"`
-	ChaewoomCriteria int     `json:"chaewoomCriteria"`
+	ChaewoomCriteria int64   `json:"chaewoomCriteria"`
 }
 
 func GetScores(c *fiber.Ctx) error {
@@ -46,18 +47,21 @@ func GetScores(c *fiber.Ctx) error {
 	queryString := `
 	select id, test_date, test_name, member_id, member_name, 
 		team, subject, teacher, err_cnt, ttl_cnt, chaewoom, 
-		reg_id, reg_date, mod_date, remarks
+		reg_id, reg_date, mod_id, mod_date, remarks
 	from ceng_test_score
-	where t_date > DATE_SUB(NOW(), INTERVAL 30 DAY)
-	order by t_date desc, team, test_name, member_name 
+	where test_date > DATE_SUB(NOW(), INTERVAL 30 DAY)
+	order by test_date desc, team, test_name, member_name 
 	`
 
 	var rr []Score
 	var r Score
 	rows, err := db.Query(queryString)
+	checkError(err)
+
 	for rows.Next() {
-		err := rows.Scan(&r.ID, &r.TestDate, &r.TestName, &r.MemberID, &r.MemberName, &r.Team, &r.Subject, &r.Teacher,
-			&r.ErrCnt, &r.TtlCnt, &r.Chaewoom, &r.RegID, &r.RegDate, &r.ModDate, &r.Remarks)
+		err := rows.Scan(&r.ID, &r.TestDate, &r.TestName, &r.MemberID, &r.MemberName,
+			&r.Team, &r.Subject, &r.Teacher, &r.ErrCnt, &r.TtlCnt, &r.Chaewoom,
+			&r.RegID, &r.RegDate, &r.ModID, &r.ModDate, &r.Remarks)
 		if err != nil {
 			fmt.Printf("log.Logger: %v\n", err.Error())
 			return c.SendString(err.Error())
@@ -97,8 +101,8 @@ func NewScore(c *fiber.Ctx) error {
 	queryString := `
 	insert into ceng_test_score (test_date, test_name, member_id, member_name, 
 		team, subject, teacher, err_cnt, ttl_cnt, chaewoom, 
-		reg_id, reg_date, mod_date, remarks)
-	values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+		reg_id, reg_date, remarks)
+	values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 	`
 
 	tx, err := db.Begin()
@@ -117,7 +121,7 @@ func NewScore(c *fiber.Ctx) error {
 		}
 		_, err = tx.Exec(queryString, s.TestDate, s.TestName, s.MemberID, s.MemberName,
 			s.Team, s.Subject, s.Teacher, s.ErrCnt, s.TtlCnt, chaewoom,
-			s.RegID, s.RegDate, s.ModDate, s.Remarks)
+			s.RegID, s.RegDate, s.Remarks)
 		checkError(err)
 
 		if err != nil {
